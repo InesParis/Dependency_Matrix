1ad789380625f51e80fee8148a588e37876b5f96
InesParis <inesparisarranz@gmail.com>
2024-11-15 14:26:30 +0100

    Update xplan.py

--- xplan.py
+++ xplan.py
@@ -1,38 +1,6 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import random
-#basic selected components
-#components = ['A', 'B', 'C', 'D']
-
-#matrix = np.zeros((len(components), len(components)))
-
-#for i in range(len(components)):
-    #matrix[i,i]=1
-
-#modifications=[('A', 'B'), ('B', 'C'), ('C', 'D')]
-
-#for mod in modifications:
-    #mod_index=(components.index(mod[0]), components.index(mod[1]))
-    #matrix[mod_index[0], mod_index[1]]=1
-    
-#plt.figure(figsize=(6,6))
-#plt.imshow(matrix, cmap="Blues", interpolation='nearest')
-
-#plt.xticks(np.arange(len(components)), components)
-#plt.yticks(np.arange(len(components)), components)
-
-
-#plt.grid(False)
-
-#plt.colorbar(label="Dependency (1:Yes, 0:No)")
-
-#plt.title("Dependency Matrix Heatmap")
-#plt.xlabel("Dependent Component")
-#plt.ylabel("Modifying Component")
-
-#plt.show()
-
-
 
 
 #random components
@@ -97,6 +65,41 @@ print("Note that the dependencies are one-way: Changing component A does not req
 
 # Show the plot
 plt.show()
+
+#basic selected components
+#components = ['A', 'B', 'C', 'D']
+
+#matrix = np.zeros((len(components), len(components)))
+
+#for i in range(len(components)):
+    #matrix[i,i]=1
+
+#modifications=[('A', 'B'), ('B', 'C'), ('C', 'D')]
+
+#for mod in modifications:
+    #mod_index=(components.index(mod[0]), components.index(mod[1]))
+    #matrix[mod_index[0], mod_index[1]]=1
+    
+#plt.figure(figsize=(6,6))
+#plt.imshow(matrix, cmap="Blues", interpolation='nearest')
+
+#plt.xticks(np.arange(len(components)), components)
+#plt.yticks(np.arange(len(components)), components)
+
+
+#plt.grid(False)
+
+#plt.colorbar(label="Dependency (1:Yes, 0:No)")
+
+#plt.title("Dependency Matrix Heatmap")
+#plt.xlabel("Dependent Component")
+#plt.ylabel("Modifying Component")
+
+#plt.show()
+
+
+#-----------------------------------------------------------------------------------
+
 
 #creation of the matrix without matplot
 #components = ['A', 'B', 'C', 'D']
